0b621ca2a9dbd8646830cdba6d0a636ac7a87813
Julia Finarovsky <juliafinarovsky@gmail.com>
2017-05-05 03:18:44 -0400

    seeded user db

--- user_models/seedUsers.js
+++ user_models/seedUsers.js
@@ -2,4 +2,24 @@ const mongoose = require('mongoose');
 const faker = require('faker')
 const {User} = require('./users');
 
+function generateUser() {
+  console.log('Seeding one user to the db');
+  User
+  .create({
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    role: randomRole(),
+    password: faker.internet.password(),
+    username: faker.internet.userName()
+  })
+}
 
+
+function randomRole() {
+
+  roles = ['student', 'teacher'];
+
+  return roles[Math.floor(Math.random() * roles.length )]
+}
+
+module.exports = generateUser
