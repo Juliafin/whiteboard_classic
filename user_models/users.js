7740d70dc539e2a6466c315898207000512ea378
Julia Finarovsky <juliafinarovsky@gmail.com>
2017-05-05 01:11:22 -0400

    added user model

---
+++ user_models/users.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const userValidation = [/^\w{4,}$/, 'Username must contain alphanumberic characters'];
+const passValidation = [/^(?=.*\d)(?=.*[A-Za-z])[a-zA-Z\d@!\?\^\\\{\}()#$%_-]{8,20}$/, 'Password must be 8 to 20 characters, and contain one letter, one number, and one special character'];
+const maxlength = [20, 'The length is longer than 20 characters.'];
+const roleValidation = [/(?=^student$)|(?=^teacher$)/, 'Role must be a teacher or a student']
+const userSchema = mongoose.Schema( {
+  username: {
+    type:String,
+    required: true,
+    minlength: 4,
+    maxlength: maxlength,
+    match: userValidation,
+    trim: true
+  },
+  password: {
+    type:String,
+    required: true,
+    minlength: 8,
+    maxlength: 18,
+    match: passValidation,
+    trim: true
+  },
+  role: {
+    type:String,
+    required: true,
+    match: roleValidation
+  }
+});
+
+userSchema.methods.showUser = function() {
+  return {
+  username: this.username,
+  role: this.role
+  }
+};
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = {User};
+
+
