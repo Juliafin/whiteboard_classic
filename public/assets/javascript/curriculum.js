cc0c729820aec19b0d1d92a96016c980a736b083
Julia Finarovsky <juliafinarovsky@gmail.com>
2017-05-11 01:59:43 -0400

    working on login pages, added some listeners

--- public/assets/javascript/curriculum.js
+++ public/assets/javascript/curriculum.js
@@ -1,3 +1,87 @@
+var state = {
+  templates: {
+    register: `<div class="signupbox">
+
+      <form class="signup">
+        <fieldset>
+          <legend for="Username and password"></legend>
+          <h1>Curriculum Manager Registration</h1>
+          
+  		  <label for="Username">Username</label>
+          <input placeholder="Username!1%" type="text" name="username" id="username">
+  
+  		   <label for="Password">Password</label>
+          <input placeholder="1234passw0rd" type="password" name="password" id="password">
+
+          <label for="First_Name">First Name</label>
+          <input type="text" name="First_Name" id="first_name">
+  
+  			<label for="Last_Name">Last Name</label>
+          <input type="text" name="Last_Name" id="last_name">
+
+          
+		<div class="teacher_student_select">
+          <label for="teacher" id="teacher_label">Teacher</label>            
+          <input type="radio" name="radSize" id="teacher" value="teacher" checked="checked">
+          <label for="student" id="student_label">Student</label>
+          <input type="radio" name="radSize" id="student" value="student">
+  </div>
+
+          <div class="button_container">	
+          <button type="submit" name="signup">Sign Up</button>
+  </div>
+         
+        </fieldset>
+        
+      </form>
+    </div>`,
+
+    login:`<div class="signupbox">
+
+      <form class="login">
+        <fieldset>
+          <legend for="Username and password"></legend>
+          <h1>Curriculum Manager Log in</h1>
+          <label for="Username">Username</label>
+          <input placeholder="Username!1%" type="text" name="username" id="username">
+
+          <label for="Password">Password</label>
+          <input placeholder="1234passw0rd" type="password" name="password" id="password">
+
+          <button id="login_button" type="submit" name="signup">Login</button>
+
+      </form>
+    </div>`
+
+
+
+  }
+
+
+
+};
+
+function authorizationFormListener() {
+
+$('nav li').click(function(event) {
+
+  if ($(this).text() === 'Register') {
+    $('div.background').empty();
+    $('div.background').html(state.templates.register);
+  }
+
+  if ($(this).text() === 'Login') {
+    $('div.background').empty();
+    $('div.background').html(state.templates.login);
+    loginSubmitListener();
+  }
+});
+
+}
+
+
+
+
 function loginSubmitListener() {
   
   $('button[type="submit"]').click(function(event) {
@@ -44,11 +128,11 @@ function register (password, username, first_name, last_name, role) {
   });
 }
 
-function sendToken () {
-  if (window.localStorage.getItem('token')) {
+// function sendToken () {
+//   if (window.localStorage.getItem('token')) {
 
-  }
-}
+//   }
+// }
 
 
 function setToken (data) {
@@ -61,7 +145,8 @@ function setToken (data) {
 
 
 ($(document).ready(function () {
-  loginSubmitListener();
+  
+  authorizationFormListener();
   $(this).scrollTop(0);
 
 
