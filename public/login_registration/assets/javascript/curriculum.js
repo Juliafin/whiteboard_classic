02739ef0188208b17dce72d991b5a1ceb7ac12da
Julia Finarovsky <juliafinarovsky@gmail.com>
2017-05-13 02:04:08 -0400

    added error validation on forms

--- public/login_registration/assets/javascript/curriculum.js
+++ public/login_registration/assets/javascript/curriculum.js
@@ -9,16 +9,16 @@ var state = {
           <h1>Curriculum Manager Registration</h1>
           
   		  <label for="Username">Username</label>
-          <input placeholder="Username!1%" type="text" name="username" id="register_username">
+          <input placeholder="Username!1%" type="text" name="username" id="register_username" required>
   
   		   <label for="Password">Password</label>
-          <input placeholder="1234passw0rd" type="password" name="password" id="register_password">
+          <input placeholder="1234passw0rd" type="password" name="password" id="register_password" required>
 
           <label for="First_Name">First Name</label>
-          <input type="text" name="First_Name" id="register_first_name">
+          <input type="text" name="first_name" id="register_first_name" required>
   
   			<label for="Last_Name">Last Name</label>
-          <input type="text" name="Last_Name" id="register_last_name">
+          <input type="text" name="last_name" id="register_last_name" required>
 
           
 		<div class="teacher_student_select">
@@ -29,7 +29,7 @@ var state = {
     </div>
 
           <div class="button_container">	
-          <button id="register_button" name="signup">Sign Up</button>
+          <button id="register_button" type="submit" name="signup">Sign Up</button>
   </div>
          
         </fieldset>
@@ -77,8 +77,8 @@ function authorizationFormListener() {
     if ($(this).text() === 'Home') {
       $(this).toggleClass('selected');
       $('div.nav li').not($(this)).removeClass('selected');
-    
-  }
+
+    }
 
     if ($(this).text() === 'Register') {
       $('div.background').empty();
@@ -117,7 +117,7 @@ function registerSubmitListener() {
 
   $('button#register_button').click(function (event) {
     event.preventDefault();
-  alert('this listener is running')
+    alert('this listener is running')
 
     var role = $('input[name="radSize"]:checked').val();
     console.log(role);
@@ -130,6 +130,8 @@ function registerSubmitListener() {
     console.log(password);
     console.log(first_name);
     console.log(last_name);
+    var result = validateFormData(password, username, first_name, last_name);
+    console.log(result);
 
   })
 
@@ -184,7 +186,83 @@ function register(password, username, first_name, last_name, role) {
   });
 }
 
-function authenticate() {
+function validateFormData(password, username, first_name, last_name) {
+  var errors = {}
+  var formData = {}
+
+  // Testing if password is valid
+  if (password) {
+    var passValid = new RegExp(/^(?=.*\d)(?=.*[A-Za-z])[a-zA-Z\d@!\?\^\\\{\}()#$%_-]{8,20}$/);
+    if (passValid.test(password) === false) {
+      errors.password = "Password must be 8 to 20 characters, and contain one letter, one number, and one special character";
+    } else {
+      formData.password = password;
+    }
+  } else if (password === "") {
+    errors.password = "Password is empty";
+  }
+
+  // Testing if username is valid
+  if (username) {
+    var usernameValid = new RegExp(/^\w{4,}$/);
+    if (usernameValid.test(username) === false) {
+      errors.username = "Username must contain only alphanumberic characters'";
+    } else {
+      formData.username = username;
+    }
+  } else if (username === "") {
+    errors.username = "Username is empty";
+  }
+
+  // Testing if first name is valid  
+  var nameValid = new RegExp(/^([ \u00c0-\u01ffa-zA-Z'\-])+$/);
+
+  if (first_name) {
+    if (nameValid.test(first_name) === false) {
+      errors.first_name = "First name must contain only UTF-8 letters and a hyphen";
+    } else {
+      formData.first_name = first_name;
+    }
+  } else if (first_name === "") {
+    errors.first_name = "First name is empty";
+  }
+
+  // Testing if last_name is valid
+
+  if (last_name) {
+    if (nameValid.test(last_name) === false) {
+      errors.last_name = "Last name must contain only UTF-8 letters and a hyphen";
+    } else {
+      formData.last_name = last_name;
+    }
+  } else if (last_name === "") {
+    errors.last_name = "Last name is empty";
+  }
+
+  // render to the DOM
+  if (Object.keys(errors).length > 0) {
+     $('.error').remove();
+    Object.keys(errors).forEach(function(elem) {
+      console.log(elem);
+      console.log(errors);
+      var currentError = errors[elem];
+      console.log( currentError);
+      var errorMsg = `<p class="error" id="${elem}_error"> ${currentError}</p>`;
+      console.log(errorMsg);
+      $(`input[name="${elem}"]`).after(errorMsg);
+    });
+    return errors;
+  } else {
+    formData.message = "No errors found";
+    return formData;
+  }
+}
+
+
+
+
+
+function authenticateToken() {
   return $.ajax({
     type: 'POST',
     url: '/auth/authenticate',
@@ -193,6 +271,9 @@ function authenticate() {
     }
   });
 }
+
+
+
 
 // function sendTokenAndRedirect() {
 
@@ -215,17 +296,17 @@ function authenticate() {
 
 function checkTokenAndAppendNav() {;
   if (window.localStorage.getItem('token')) {
-    authenticate().
+    authenticateToken().
     then(function (userdata) {
-      console.log(userdata.url);
-      window.localStorage.setItem('current_user', userdata.username);
-      window.localStorage.setItem('dashboard_url', userdata.url);
+        console.log(userdata.url);
+        window.localStorage.setItem('current_user', userdata.username);
+        window.localStorage.setItem('dashboard_url', userdata.url);
 
-      appendLoggedinNav();
-    })
-    .catch(function(err) {
-      console.log(err);
-    })
+        appendLoggedinNav();
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
 
   }
 }
@@ -255,8 +336,8 @@ function navbarActiveListener() {
 
   // $('div.nav li').click(function (event) {
 
-    $(this).toggleClass('selected');
-    $('div.nav li').not($(this)).removeClass('selected');
+  $(this).toggleClass('selected');
+  $('div.nav li').not($(this)).removeClass('selected');
   // });
 }
 
